498a3a1732046c04726174f43c23937fab881c96
Carlos20daniel <1440648@senati.pe>
2023-09-22 11:43:09 -0500

    actualizar capa_negocio

--- Capa_Negocio/ClassNegocio.cs
+++ Capa_Negocio/ClassNegocio.cs
@@ -13,5 +13,18 @@ namespace Capa_Negocio
     public class ClassNegocio
     {
         ClassDatos objd = new ClassDatos();
+
+        public DataTable N_listar_clientes()
+        {
+            return objd.D_listar_clientes();
+        }
+        public DataTable N_buscar_clientes(ClassEntidad obje)
+        {
+            return objd.D_buscar_clientes(obje);
+        }
+        public String N_mantenimiento_cliente(ClassEntidad obje)
+        {
+            return objd.D_mantenimiento_clientes(obje);
+        }
     }
 }
