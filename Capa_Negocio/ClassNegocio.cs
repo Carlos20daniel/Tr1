44bfa3ee11c109cd1372ce54887911b5398cc622
Carlos20daniel <1440648@senati.pe>
2023-09-22 11:33:33 -0500

    actualizar capa_entidad

--- Capa_Negocio/ClassNegocio.cs
+++ Capa_Negocio/ClassNegocio.cs
@@ -4,9 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using System.Data;
+using Capa_Datos;
+using Capa_Entidad;
+
 namespace Capa_Negocio
 {
     public class ClassNegocio
     {
+        ClassDatos objd = new ClassDatos();
     }
 }
